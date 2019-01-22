4381ef07a18ca772f484671102eb58a7459850c4
gurimusan <gurimusan@gmail.com>
2019-01-22 18:48:24 +0900

    python用のシグナリングサーバの実装を追加

---
+++ signaling-server/server/python/signaling.py
@@ -0,0 +1,35 @@
+import asyncio
+import websockets
+import json
+
+CLIENTS = set()
+
+
+async def notify(sender, message):
+    clients = [c for c in CLIENTS if c != sender]
+    for c in clients:
+        await c.send(message)
+
+
+async def register_client(ws):
+    CLIENTS.add(ws)
+
+
+async def unregister_client(ws):
+    CLIENTS.remove(ws)
+
+
+async def handler(ws, path):
+    await register_client(ws)
+    try:
+        async for message in ws:
+            print(f"< {message}")
+            await notify(ws, message)
+    finally:
+        await unregister_client(ws)
+
+
+start_server = websockets.serve(handler, None, 3001)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
